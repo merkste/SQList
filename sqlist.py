2ce0f4a0068f35aa271179737e1678ee83d933f1
Steffen Märcker <maercker@tcs.inf.tu-dresden.de>
2015-08-25 17:18:29 +0200

    Add single-line docstrings to API functions

--- sqlist.py
+++ sqlist.py
@@ -4,39 +4,16 @@ from operator import itemgetter
 # SELECT('*', FROM=persons, WHERE={id: 1, age:2})
 # SELECT('*', FROM=persons, WHERE=(OR(NOT({id: 1, age:2}), {age: (lambda age: <=2))}))
 def SELECT(COLUMNS, FROM, WHERE=None):
+  """Similarily to SQL, select and project all rows from a table matching the constraints."""
   table = select_rows(FROM, WHERE) if WHERE else FROM
   if COLUMNS in ['*', (), None]:
     return table
   return project_columns(table, COLUMNS)
 
-def project_columns(table, columns):
-  rows = iter(table)
-  head = rows.next()
-  indices = get_column_indices(head);
-  getter = itemgetter(*(indices[col] for col in columns))
-  return imap(getter, chain([head], rows))
-
-def select_rows(table, column_spec):
-  if isinstance(column_spec, dict):
-    return select_rows(table, AND(column_spec))
-  bind_matchers = column_spec
-  rows = iter(table)
-  head = rows.next()
-  
-  # 1. resolve column indices
-  indices = get_column_indices(head)  
-  # 2. bind matchers
-  matchers = bind_matchers(indices) if bind_matchers else (lambda row: True)
-  # 3. filter
-  return chain([head], ifilter(matchers, rows))
-
-def get_column_indices(head):
-  return {head[index] : index for index in xrange(0, len(head))}
-
-
 # NOT: dict -> indices -> (indices -> row -> boolean)
 # NOT: (indices -> row -> boolean) -> (indices -> row -> boolean)
 def NOT(column_specs):
+  """Negate a column_spec and return a binding function."""
   if callable(column_specs):
     # indices -> row -> boolean
     return lambda indices: not_matcher(column_specs, indices)
@@ -44,25 +21,54 @@ def NOT(column_specs):
     # dict
     return NOT(AND(column_specs))
 
-def not_matcher(bind_matchers, indices):
-  matcher = bind_matchers(indices) # bind column indices
-  return lambda row: not matcher(row)
-
 # AND: (indices -> row -> boolean)+ -> (indices -> row -> boolean)
 # AND: dict+ -> (indices -> row -> boolean)
 def AND(*column_specs):
+  """Apply logical AND to a list of column_specs and return a binding function."""
   bind_matchers = as_binding_functions(column_specs)
   return lambda indices: and_matcher(bind_matchers, indices)
-
-def and_matcher(bind_matchers, indices):
-  matchers = tuple(bind_matcher(indices) for bind_matcher in bind_matchers)  # bind column indices
-  return lambda row: all(matcher(row) for matcher in matchers);
 
 # OR: (indices -> row -> boolean)+ -> (indices -> row -> boolean)
 # OR: dict+ -> (indices -> row -> boolean)
 def OR(*column_specs):
+  """Apply logical OR to a list of column_specs and return a binding function."""
   bind_matchers = as_binding_functions(column_specs)
   return lambda indices: or_matcher(bind_matchers, indices)
+
+def project_columns(table, columns):
+  """Project the table to the specified columns."""
+  rows = iter(table)
+  head = rows.next()
+  indices = column_indices(head);
+  getter = itemgetter(*(indices[col] for col in columns))
+  return imap(getter, chain([head], rows))
+
+def select_rows(table, column_spec):
+  """Select all rows which entries match the column_spec."""
+  if isinstance(column_spec, dict):
+    return select_rows(table, AND(column_spec))
+  bind_matchers = column_spec
+  rows = iter(table)
+  head = rows.next()
+  
+  # 1. resolve column indices
+  indices = column_indices(head)  
+  # 2. bind matchers
+  matchers = bind_matchers(indices) if bind_matchers else (lambda row: True)
+  # 3. filter
+  return chain([head], ifilter(matchers, rows))
+
+def column_indices(head):
+  """Build a dictionary of the column indices."""
+  return {head[index] : index for index in xrange(0, len(head))}
+
+def not_matcher(bind_matchers, indices):
+  matcher = bind_matchers(indices) # bind column indices
+  return lambda row: not matcher(row)
+
+def and_matcher(bind_matchers, indices):
+  matchers = tuple(bind_matcher(indices) for bind_matcher in bind_matchers)  # bind column indices
+  return lambda row: all(matcher(row) for matcher in matchers);
 
 def or_matcher(bind_matchers, indices):
   matchers = tuple(bind_matcher(indices) for bind_matcher in bind_matchers)  # bind column indices
